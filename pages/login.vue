63ddcc8c785f40e8880a240d766b0ba47a246ffb
Terry Tilley <terry@terrytilley.com>
2018-08-08 23:03:35 +0100

    error message forms

--- pages/login.vue
+++ pages/login.vue
@@ -4,16 +4,21 @@
       <el-col :span="8">
         <div>
           <h1>Login</h1>
-          <el-form ref="form" :model="form">
-            <el-form-item label="Email">
+          <el-form ref="loginForm" :model="form" :rules="rules">
+            <el-form-item label="Email" prop="email">
               <el-input v-model="form.email" type="email" />
             </el-form-item>
-            <el-form-item label="Password">
+            <el-form-item label="Password" prop="password">
               <el-input v-model="form.password" type="password" />
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="onSubmit">Submit</el-button>
+              <el-button type="primary" @click="submitForm('loginForm')">Submit</el-button>
             </el-form-item>
+            <el-alert
+              v-if="errorMessage"
+              :title="errorMessage"
+              :closable="false"
+              type="error"/>
           </el-form>
         </div>
       </el-col>
@@ -29,28 +34,52 @@ export default {
   middleware: 'public',
   data() {
     return {
+      errorMessage: '',
       form: {
         email: '',
         password: '',
       },
+      rules: {
+        email: [
+          { type: 'email', message: 'Not a valid email address', trigger: 'blur' },
+          { required: true, message: 'Please input your email', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: 'Please input your password', trigger: 'blur' },
+          {
+            min: 8,
+            message: 'Password should be at least 8 characters long',
+            trigger: 'blur',
+          },
+        ],
+      },
     }
   },
   methods: {
-    async onSubmit() {
-      const variables = this.form
+    submitForm(formName) {
+      this.$refs[formName].validate(async valid => {
+        const variables = this.form
 
-      try {
-        const token = await this.$apollo
-          .mutate({ mutation, variables })
-          .then(({ data }) => data && data.login)
+        if (valid) {
+          try {
+            const token = await this.$apollo
+              .mutate({ mutation, variables })
+              .then(({ data }) => data && data.login)
 
-        await this.$apolloHelpers.onLogin(token)
+            await this.$apolloHelpers.onLogin(token)
 
-        this.$store.commit('auth/isAuthUpdate', true)
-        this.$router.push('/')
-      } catch (e) {
-        console.error(e)
-      }
+            this.$store.commit('auth/isAuthUpdate', true)
+            this.$router.push('/')
+          } catch (e) {
+            this.errorMessage = e.graphQLErrors[0].message
+          }
+        }
+
+        return false
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
     },
   },
 }
--- pages/signup.vue
+++ pages/signup.vue
@@ -4,19 +4,24 @@
       <el-col :span="8">
         <div>
           <h1>Sign Up</h1>
-          <el-form ref="form" :model="form">
-            <el-form-item label="Username">
+          <el-form ref="signUpForm" :model="form" :rules="rules">
+            <el-form-item label="Username" prop="username">
               <el-input v-model="form.username" type="text" />
             </el-form-item>
-            <el-form-item label="Email">
+            <el-form-item label="Email" prop="email">
               <el-input v-model="form.email" type="email" />
             </el-form-item>
-            <el-form-item label="Password">
+            <el-form-item label="Password" prop="password">
               <el-input v-model="form.password" type="password" />
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="onSubmit">Submit</el-button>
+              <el-button type="primary" @click="submitForm('signUpForm')">Submit</el-button>
             </el-form-item>
+            <el-alert
+              v-if="errorMessage"
+              :title="errorMessage"
+              :closable="false"
+              type="error"/>
           </el-form>
         </div>
       </el-col>
@@ -32,29 +37,61 @@ export default {
   middleware: 'public',
   data() {
     return {
+      errorMessage: '',
       form: {
         username: '',
         email: '',
         password: '',
       },
+      rules: {
+        username: [
+          { required: true, message: 'Please input a username', trigger: 'blur' },
+          {
+            min: 3,
+            message: 'Username should be at least 3 characters long',
+            trigger: 'blur',
+          },
+        ],
+        email: [
+          { type: 'email', message: 'Not a valid email address', trigger: 'blur' },
+          { required: true, message: 'Please input your email', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: 'Please input a password', trigger: 'blur' },
+          {
+            min: 8,
+            message: 'Password should be at least 8 characters long',
+            trigger: 'blur',
+          },
+        ],
+      },
     }
   },
   methods: {
-    async onSubmit() {
-      const variables = this.form
+    submitForm(formName) {
+      this.$refs[formName].validate(async valid => {
+        const variables = this.form
 
-      try {
-        const token = await this.$apollo
-          .mutate({ mutation, variables })
-          .then(({ data }) => data && data.signup)
+        if (valid) {
+          try {
+            const token = await this.$apollo
+              .mutate({ mutation, variables })
+              .then(({ data }) => data && data.signup)
 
-        await this.$apolloHelpers.onLogin(token)
+            await this.$apolloHelpers.onLogin(token)
 
-        this.$store.commit('auth/isAuthUpdate', true)
-        this.$router.push('/')
-      } catch (e) {
-        console.error(e)
-      }
+            this.$store.commit('auth/isAuthUpdate', true)
+            this.$router.push('/')
+          } catch (e) {
+            this.errorMessage = e.graphQLErrors[0].message
+          }
+        }
+
+        return false
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
     },
   },
 }
